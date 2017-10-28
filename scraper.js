75d01ae4ec0c3333a3c0ebf15daef8c8a3034fa3
Chris <cli53@binghamton.edu>
2017-10-27 23:41:50 -0400

    Parsing page.$$ obj

--- scraper.js
+++ scraper.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 const CREDS = require('./creds');
+const util = require('util');
+
 // const moongoose = require('mongoose');
 // const User = require('./models/user'); 
 
@@ -12,9 +14,13 @@ async function run() {
   // now we need to instantiate a new page 
   const page = await browser.newPage();
 
+  page.setViewport({
+    width: 1920,
+    height: 1080
+});
+
   await page.goto('https://www.indeed.com/');
 
-  await page.viewport({width: 1000, height: 1000})
 
   // Signing in: 
   // setting sign-in link string id to a variable
@@ -55,23 +61,38 @@ async function run() {
  
 // iterate based on # 
 
-  console.log('hello')
+//   console.log('hello')
 
-  const LIST_LINK = '#resultsCol > div.row.result:nth-child(3) > h2 > a';
-  const LENGTH_SELECTOR_ID = 'div[data-tn-component]';
+//   const LIST_LINK = '#resultsCol > div.row.result:nth-child(3) > h2 > a';
+//   const LENGTH_SELECTOR_ID = 'div[data-tn-component]';
 //   console.log(LIST_LINK)
-const rowResult = '.row.result'
+const resultsCol = '#resultsCol'
+const showing = '.row.result'
+const link = '.turnstileLink'
+const showing2 = '.showing'
 //   const LIST_LINK = '#resultsCol > div.row.result:nth-child(INDEX) > h2 > a';
-  console.log('its hits')
-
-  let link = await page.evaluate((sel)=> {
-      console.log('THIS IS SEL',sel)
-      return document.find(sel)
-  }, LENGTH_SELECTOR_ID);
-
-  console.log(link)
+//   console.log('its hits')
 
 
+// const result = await page.$$eval(showing, divs => console.log(divs))
+// const findIt = await page.$$(showing2)
+const obj = await page.$$(showing)
+
+console.log(obj)
+// await findIt.click();
+// console.log('THIS IS FINDIT',findIt)
+// console.log('THIS IS UTIL ==>>>>>>>', util.inspect(findIt, false, null))
+
+// error: Converting circular structure to JSON
+//   let link = await page.$$eval((sel)=> {
+//       console.log('THIS IS SEL',sel)
+//       return sel
+//   }, findIt);
+
+
+  
+  
+//*[@id="p_67d093d34e1ac778"]
 
   //loop through number of pages, all of the outer loop would be in the inner loop
 //   let listLength 
